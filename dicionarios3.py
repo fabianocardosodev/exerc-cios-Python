2230d04952ef732d893bf00b993fbbb90b70a2b9
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-13 00:10:30 -0300

    alteração em exerc3

--- dicionarios3.py
+++ dicionarios3.py
@@ -2,11 +2,11 @@ favorite_languages = {
     'jen' : 'Python',
     'sarah' : 'c', 
     'edward' : 'ruby',
-    'phil' : 'pyhton'
+    'phil' : 'Python'
 }
 
-# O METODO KEYS UTIL QUANDO NAO NECESSITAMOS TRABALHAR 
-# COM TODOS OS VALORES,POREM VOCE PODE EXPLICITA-LO OU NAO
+# O METODO KEYS E UTIL QUANDO NAO NECESSITAMOS TRABALHAR 
+# COM TODOS OS VALORES , POREM VOCE PODE EXPLICITA-LO OU NAO
 # POIS O CODIGO TRABALHARA DA MESMA FORMA
 
 for name in favorite_languages.keys():
