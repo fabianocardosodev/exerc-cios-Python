b29474793d2bd60478ac4fd4bad487d675df1ffc
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-13 00:05:47 -0300

    criei exerc dicionarios3

---
+++ dicionarios3.py
@@ -0,0 +1,12 @@
+favorite_languages = {
+    'jen' : 'Python',
+    'sarah' : 'c', 
+    'edward' : 'ruby',
+    'phil' : 'pyhton'
+}
+
+# O METODO KEYS UTIL QUANDO NAO NECESSITAMOS TRABALHAR 
+# COM TODOS OS VALORES
+
+for name in favorite_languages.keys():
+    print(name.title())
