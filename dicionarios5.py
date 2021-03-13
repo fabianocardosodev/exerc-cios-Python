521a82de820ad658d1d6148327b8f89028d3243b
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-13 14:14:08 -0300

    exercicios dicionarios

---
+++ dicionarios5.py
@@ -0,0 +1,13 @@
+favorite_languages = {
+    'jen' : 'Python',
+    'sarah' : 'c', 
+    'edward' : 'ruby',
+    'phil' : 'Python'
+}
+
+# para descobrir se uma pessoa em particular participou da enquete.
+
+if 'Alex' not in favorite_languages.keys():
+    print('Desculpe seu nome nao está na lista')
+
+    
