a827fe57b4c7677c415c37256667d93603bec0f4
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-13 00:08:23 -0300

    alteração em dicionarios3

--- dicionarios3.py
+++ dicionarios3.py
@@ -6,7 +6,8 @@ favorite_languages = {
 }
 
 # O METODO KEYS UTIL QUANDO NAO NECESSITAMOS TRABALHAR 
-# COM TODOS OS VALORES
+# COM TODOS OS VALORES,POREM VOCE PODE EXPLICITA-LO OU NAO
+# POIS O CODIGO TRABALHARA DA MESMA FORMA
 
 for name in favorite_languages.keys():
     print(name.title())
