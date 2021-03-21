30c7b4b143dd664ee54004cfb2dd96c6c55fa9b3
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-21 11:52:28 -0300

    exercicio dicionario
    cria uma lista vazia
    cria 30 aliens e armazena caracteristicas no dicionario e inclui eles na lista
    altera 3 primeiros
    armazena outra alteração 3
    imprimi o 5
    e depois quantos criados

---
+++ dicionarios12.py
@@ -0,0 +1,26 @@
+#cria uma lista vazia para armazenar
+aliens = []
+
+#cria 30 aliens e inclui na lista
+for alien_number in range(0,30):
+    new_alien = { 'colors' : 'green' , 'points' : 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+#altera 3 primeiros aliens e depois altera novamente
+for alien in aliens[0:3]:
+    if alien ['colors'] == 'green':
+        alien['colors'] = 'blue'
+        alien['points'] = 10
+        alien['speed'] = 'medium'
+    elif alien ['colors'] == 'blue':
+        alien ['colors'] = 'yellow'
+        alien ['points'] = 15
+        alien ['speed'] = 'fast'
+
+#mostra os 5 primeiros aliens
+for alien in aliens[0:5]:
+    print(alien)
+print("...")
+
+# Mostra quantos alienígenas foram criados 
+print( "Numero total de aliens criados: " + str(len(aliens)))
