82e111d34ed73102fb28fea6fa53e8976a287b6e
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-21 11:15:27 -0300

    exercicio dicionarios
    Criar dicionarios, reune em uma lista que contenha os dicionarios e por fim percorre a lista com laço

---
+++ dicionarios9.py
@@ -0,0 +1,13 @@
+alien_0 = { 'colors' : 'green' , 'points': 5}
+alien_1 = { 'colors' : 'blue' , 'points' : 7}
+alien_2 = { 'colors' : 'white' , 'points' : 8}
+
+aliens = [alien_0,alien_1,alien_2]
+
+for alien in aliens:
+    print(alien)
+
+# Inicialmente criamos três dicionários,
+# cada um representando um alienígena diferente.
+# Depois reunimos esses dicionários em uma lista chamada aliens. 
+# Por fim, percorremos a lista com um laço e exibimos cada alienígena
