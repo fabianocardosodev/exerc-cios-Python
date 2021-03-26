dd2396b85e66a85f4fb03f59748e5ce3fc6d47f5
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-03-25 22:58:20 -0300

    exercicio dicionario2 tarefa

---
+++ exerdiciona5.py
@@ -0,0 +1,9 @@
+fera = {'animal':'pitbull', 'dono':'Flavio José'}
+bocudo = {'animal':'papagaio','dono':'Priscila Alcantara'}
+mimi = {'animal': 'gata','dono':'Fabrine Cardoso'}
+sara = {'animal': 'pastor alemao','dono' : 'Fabiano Cardoso'}
+
+pets = [fera,bocudo,mimi,sara]
+
+for pet in pets:
+    print('\nPets: ' , pet)
