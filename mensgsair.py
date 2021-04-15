cdc36d37db57d2e2c6a7bbf87a2457aaa3e855de
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-04-14 23:22:18 -0300

    alteração
    Definiremos um valor de saída e então deixaremos o programa executando

--- mensgsair.py
+++ mensgsair.py
@@ -1,3 +1,6 @@
+#Definiremos um valor de saída e então deixaremos o programa executando 
+# enquanto o usuário não tiver fornecido o valor de saída "fim"
+
 prompt = "\nDiga-me alguma coisa e eu repetirei para você!"
 prompt += "\nEnter para o fim do programa "
 
