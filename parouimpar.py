d914a82b755edd6b3a87f774874afce5e7f79f5a
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-04-14 22:09:27 -0300

    alteração exercicio input

--- parouimpar.py
+++ parouimpar.py
@@ -3,6 +3,6 @@ number = int(number)
 #Com a entrada sendo ref numericamente a usamos para operações matematicas, 
 # operador modulo para saber o resto. 
 if number % 2 == 0:
-    print("Esse numero é par.")
+    print("Esse numero " + str(number) + " é par.")
 else:
-    print("Esse numero é impar.")
+    print("Esse numero " + str(number) + " é impar.")
