0b6fb5b2ddef5f4b6b8175a5576419467d5749de
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-04-14 22:44:29 -0300

    usando while para contagem
    laço while para contar uma série de números

---
+++ contagemwhil.py
@@ -0,0 +1,7 @@
+# Podemos usar um laço while para contar uma série de números. 
+# Por exemplo, o laço while a seguir conta de 1 a 5
+number_atual = 1
+while number_atual <= 5:
+    print(number_atual)
+    number_atual += 1  #O operador += é um atalho para numero_atual = numero_atual + 1
+     
