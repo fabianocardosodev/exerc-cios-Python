d527d5f8799c4233ab8ae43a3b425b1cedcc0ea2
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-04-14 23:20:01 -0300

    usando while

---
+++ mensgsair.py
@@ -0,0 +1,7 @@
+prompt = "\nDiga-me alguma coisa e eu repetirei para você!"
+prompt += "\nEnter para o fim do programa "
+
+message = " "
+while message != "fim":
+    message=input(prompt)
+    print(message)
