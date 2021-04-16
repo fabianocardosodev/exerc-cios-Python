4744751357f18ca501d3168d96c88a0537a5fa58
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-04-15 22:37:54 -0300

    alteração

--- mensgsair.py
+++ mensgsair.py
@@ -2,9 +2,9 @@
 # enquanto o usuário não tiver fornecido o valor de saída "fim"
 
 prompt = "\nDiga-me alguma coisa e eu repetirei para você!"
-prompt += "\nEnter para o fim do programa "
+prompt += "\nEnter sair para o fim do programa "
 
 message = " "
-while message != "fim":
+while message != "sair":
     message=input(prompt)
     print(message)
