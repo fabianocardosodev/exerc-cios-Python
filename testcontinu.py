635cb87d778fb98a9670df65668a4adcdbda2736
Fabiano Cardoso <fabiscardosocontato@gmail.com>
2021-04-22 16:04:27 -0300

    alteração

--- testcontinu.py
+++ testcontinu.py
@@ -1,5 +1,6 @@
 #considere um laço que conte de 1 a 10, 
 # mas apresente apenas os números ímpares desse intervalo
+# a instrução continue diz a Python para ignorar o restante do laço e voltar ao início
 
 numero_atual = 0
 while numero_atual < 10:
